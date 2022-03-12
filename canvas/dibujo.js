d0ea5e6e2ae24b2ba66eab47101c1378429e7bff
Malomx <malopezb.93s@gmail.com>
2022-03-12 07:52:18 -0600

    Se comienza clase de canvas

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -1,6 +1,8 @@
 var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 
+dibujarLinea("pink",10,300,220,10);
+dibujarLinea("blue",220,10,150,55);
 
 function dibujarLinea(color, xinicial, yinicial, xfinal, yfinal)
     {
