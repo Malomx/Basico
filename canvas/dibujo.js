9dd55babb31fd05a6f207f080f424e898e82a242
Malomx <malopezb.93s@gmail.com>
2022-03-14 20:03:16 -0600

    Si reemplazan ciclos por FOR

--- canvas/dibujo.js
+++ canvas/dibujo.js
@@ -2,28 +2,21 @@ var d = document.getElementById("dibujito");
 var lienzo = d.getContext("2d");
 var lineas = 30,l=0,x1,y1,x2,y2;
 
-while(l < lineas){
+for(l=0;l < lineas;l++){
     x1=0,y1=300-(l*10),x2=10*(l+1),y2=0;
     dibujarLinea("red",x1,y1,x2,y2);
-    l=l+1;
 }
-l=0;
-while(l < lineas){
+for(l=0;l < lineas;l++){
     x1=0,y1=10*l,x2=10*(l+1),y2=300;
     dibujarLinea("green",x1,y1,x2,y2);
-    l=l+1;
 }
-l=0;
-while(l < lineas){
+for(l=0;l < lineas;l++){
     x1=300,y1=l*10,x2=300-(l+1)*10,y2=300;
     dibujarLinea("blue",x1,y1,x2,y2);
-    l=l+1;
 }
-l=0;
-while(l < lineas){
+for(l=0;l < lineas;l++){
     x1=l*10,y1=0,x2=300,y2=10*(l+1);
     dibujarLinea("yellow",x1,y1,x2,y2);
-    l=l+1;
 }
 dibujarLinea("black",1,1,1,299);
 dibujarLinea("black",299,299,299,1);
