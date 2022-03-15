c6c5d1913a4d59a774e3e978c77af672f07e4673
Malomx <malopezb.93s@gmail.com>
2022-03-15 17:44:21 -0600

    Se añaden las respuestas con IF al telclear con flechas

--- teclas/eventos.js
+++ teclas/eventos.js
@@ -1,7 +1,32 @@
 document.addEventListener("keyup", dibujaTeclado);
+var teclas = {
+    UP: 38,
+    DOWN: 40,
+    LEFT: 37,
+    RIGHT: 39
+};
+//console.log (teclas);
+
 var d=document.getElementById("Dibuja");
 var lienzo = d.getContext("2d");
 
-function dibujaTeclado(evento){
-    console.log(evento.keyCode);
+function dibujaTeclado(evento)
+{
+    if(evento.keyCode == teclas.UP)
+    {
+        console.log("Vamoh' pa'rriba");
+    }
+    if(evento.keyCode == teclas.DOWN)
+    {
+        console.log("Vamoh' pa'bajo");
+    }
+    if(evento.keyCode == teclas.LEFT)
+    {
+        console.log("Vamoh' pa'la Izquierda");
+    }
+    if(evento.keyCode == teclas.RIGHT)
+    {
+        console.log("Vamoh' pa'lah deresha");
+    }
+
 }
